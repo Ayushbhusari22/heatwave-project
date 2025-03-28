c3ab7a305012ce3136608dddaab95820ba38988d
Ayushbhusari22 <bhusari.ayush22@gmail.com>
2025-03-28 11:55:08 +0530

    Add changes despite line ending differences

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {visualizer} from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),visualizer()],
   server: {
     proxy: {
       '/api': {
@@ -11,5 +12,8 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/,'')
       }
     }
+  },
+  build: {
+    chunkSizeWarningLimit: 600
   }
 })
